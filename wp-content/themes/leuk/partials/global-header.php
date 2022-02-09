cc3142052530799d96666888e3694cf835f2b829
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-02-09 10:09:54 +0000

    Change logo per page

--- wp-content/themes/leuk/partials/global-header.php
+++ wp-content/themes/leuk/partials/global-header.php
@@ -5,12 +5,20 @@
 $logoprimary = get_field( 'logo_primary', 'options');
 $logoteleport = get_field( 'logo_teleport', 'options');
 $logodatacentre = get_field( 'logo_datacentre', 'options');
+$logo = get_field( 'logo');
 ?>
 
 <header class="global-header">
 	<div class="global-header__inner">
-		<div class="global-header__logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo $logoprimary['url']; ?>" alt="<?php echo $logoprimary['alt']; ?>" /></a></div>
-		
+
+		<div class="global-header__logo"><a href="<?php echo home_url(); ?>">
+		<?php if ( $logo == 'logo-teleport' ) { ?>
+			<img src="<?php echo $logoteleport['url']; ?>" alt="<?php echo $logoteleport['alt']; ?>" />
+		<?php } else { ?>
+			<img src="<?php echo $logodatacentre['url']; ?>" alt="<?php echo $logodatacentre['alt']; ?>" />
+		<?php } ?>
+		</a></div>
+
 		<nav class="global-header--nav">
 			<?php wp_nav_menu( array('theme_location' => 'primary') ); ?>
 		</nav>
