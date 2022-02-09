aa1e51bbc5459c80b07cfd6a268f54930d9d319a
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-02-09 16:20:35 +0000

    Fix author name

--- wp-content/themes/leuk/blocks/posts.php
+++ wp-content/themes/leuk/blocks/posts.php
@@ -12,25 +12,21 @@
 		'orderby' => 'date',
 		'order' => 'DESC',
 	));
-	// $fname = get_the_author_meta('first_name');
-	// $lname = get_the_author_meta('last_name');
-	$author_id = get_post_field ('post_author', get_the_ID());
-	$name = get_the_author_meta( 'display_name' , $author_id );
-	// $full_name = $fname + $lname;
 ?>
 
 <?php if ($posts->have_posts()) : ?>
 <section class="posts">
-<?php while($posts->have_posts()) : $posts->the_post(); ?>
+<?php while($posts->have_posts()) : $posts->the_post();
+	$author_id=$posts->post_author;
+	$name = get_the_author_meta( 'display_name' , $author_id );
+ ?>
 	<a href="<?php esc_url( the_permalink() ); ?>" class="posts-item" title="Article: <?php the_title(); ?>">
 		<div class="posts-item--image">
 			<?php echo get_the_post_thumbnail(); ?>
 		</div>
 		<div class="posts-item--content">
-		<?php $author_id=$posts->post_author; ?>
-<?php the_author_meta( 'display_name' , $author_id ); ?>
 
-			<div class="posts-item--meta"><p><?php echo get_the_date('d.m.y'); ?></p>&nbsp;<p>/</p>&nbsp;<p>b<?php echo $name; ?></p></div>
+			<div class="posts-item--meta"><p><?php echo get_the_date('d.m.y'); ?></p>&nbsp;<p>/</p>&nbsp;<p><?php echo $name; ?></p></div>
 			<h4><strong><?php the_title(); ?></strong></h4>
 		</div>
 
