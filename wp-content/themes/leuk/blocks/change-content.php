d0809d43d7c1e60b17c2d4c9898450c312e8f3dd
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-02-13 16:17:48 +0000

    Update content

--- wp-content/themes/leuk/blocks/change-content.php
+++ wp-content/themes/leuk/blocks/change-content.php
@@ -11,6 +11,6 @@ $content2 = get_field('contentswitcher_content_2');
 <div class="change-content">
 	<div class="link link-one"><?php if ( $logo1  ) { ?><img loading="lazy" src="<?php echo $logo1['url']; ?>" alt="<?php echo $logo1['alt']; ?>" /><?php } ?></div>
 	<div class="link link-two"><?php if ( $logo2  ) { ?><img loading="lazy" src="<?php echo $logo2['url']; ?>" alt="<?php echo $logo2['alt']; ?>" /><?php } ?></div>
-	<?php if ( $content1 ) { ?><div class="content content-one"><?php echo $content1 ?></div><?php } ?>
-	<?php if ( $content2 ) { ?><div class="content content-two"><?php echo $content2 ?></div><?php } ?>
+	<?php if ( $content1 ) { ?><div class="change-content--content content-one"><?php echo $content1 ?></div><?php } ?>
+	<?php if ( $content2 ) { ?><div class="change-content--content content-two"><?php echo $content2 ?></div><?php } ?>
 </div>
--- wp-content/themes/leuk/single.php
+++ wp-content/themes/leuk/single.php
@@ -1,13 +1,12 @@
 <?php get_header(); ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
-
-	<article>
-		<h2><?php the_title(); ?></h2>
+<h2><?php the_title(); ?></h2>
 		<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time>
+	<article>
 		<?php the_content(); ?>
 	</article>
-	
+
 <?php endwhile; ?>
 
 <?php get_footer(); ?>
