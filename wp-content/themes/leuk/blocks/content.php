ae77472956fc5500227f4bd7a65d16b29387df08
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-02-18 15:17:35 +0000

    Add ID

--- wp-content/themes/leuk/blocks/content.php
+++ wp-content/themes/leuk/blocks/content.php
@@ -3,8 +3,9 @@
 * The template used for displaying a content block.
 */
 $content = get_field('content');
+$linkid = get_field('link_id');
 ?>
 
-<section class="content">
+<section class="content"<?php if ($linkid) { ?> id="<?php echo $linkid; ?>"<?php } ?>>
 	<?php echo $content; ?>
 </section>
