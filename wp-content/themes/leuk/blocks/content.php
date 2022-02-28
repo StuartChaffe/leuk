1500bc0c3d68604fffe0b11427e27a335fd44e2f
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-02-28 17:23:52 +0000

    Add small padding option

--- wp-content/themes/leuk/blocks/content.php
+++ wp-content/themes/leuk/blocks/content.php
@@ -4,8 +4,9 @@
 */
 $content = get_field('content');
 $linkid = get_field('link_id');
+$padding = get_field('content_padding');
 ?>
 
-<section class="content"<?php if ($linkid) { ?> id="<?php echo $linkid; ?>"<?php } ?>>
+<section class="content<?php if ( $padding == 'padding-small' ) { ?> content--padding__small<?php } ?>"<?php if ($linkid) { ?> id="<?php echo $linkid; ?>"<?php } ?>>
 	<?php echo $content; ?>
 </section>
