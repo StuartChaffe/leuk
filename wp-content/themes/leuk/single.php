8ff7dde632169ef9180316817d21a0dc169c4e85
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-09-09 15:33:44 +0100

    Add mobile image

--- wp-content/themes/leuk/single.php
+++ wp-content/themes/leuk/single.php
@@ -4,15 +4,18 @@
 
 <?php
 	$name = get_the_author_meta( 'display_name' );
+	$image = get_field('banner_image');
 ?>
 
-<section class="banner banner-post" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>')">
+<section class="banner banner-post" <?php if ( $image['banner_image_large'] ) { ?>style="background-image: url('<?php echo $image['banner_image_large']['url']; ?>')"<?php } ?>>
 	<div class="banner--content">
 		<div class="banner--text">
 			<h1 class="heading-2"><?php the_title(); ?></h1>
 			<div class="posts-item--meta"><p><?php echo get_the_date('d.m.y'); ?></p>&nbsp;<p>/</p>&nbsp;<p><?php echo $name; ?></p></div>
 		</div>
 	</div>
+	<?php if ( $image['banner_image_large'] ) { ?><img loading="lazy" class="banner--image hidemobile" src="<?php echo $image['banner_image_large']['url']; ?>" alt="<?php echo $image['banner_image_large']['alt']; ?>" /><?php } ?>
+	<?php if ( $image['banner_image_mobile'] ) { ?><img loading="lazy" class="banner--image hidedesktop" src="<?php echo $image['banner_image_mobile']['url']; ?>" alt="<?php echo $image['banner_image_mobile']['alt']; ?>" /><?php } ?>
 </section>
 
 <article>
