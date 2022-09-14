de91e419c323559f889325deab24e47d6f8c1c69
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-09-14 17:13:10 +0100

    Fix banner

--- wp-content/themes/leuk/blocks/banner.php
+++ wp-content/themes/leuk/blocks/banner.php
@@ -3,13 +3,14 @@
 * The template used for displaying a banner.
 */
 $image = get_field('banner_image');
+$img = get_field('images');
 $content = get_field('banner_content');
 $button = get_field('banner_button');
 $form = get_field('banner_form', false, false);
 $size = get_field('banner_size');
 ?>
 
-<section class="banner<?php if ( $size == 'banner__small' ) { ?> banner__small<?php } ?>" <?php if ( $image['banner_image_large'] ) { ?>style="background-image: url('<?php echo $image['banner_image_large']['url']; ?>')"<?php } ?>>
+<section class="banner<?php if ( $size == 'banner__small' ) { ?> banner__small<?php } ?>" <?php if ( $img ) { ?>style="background-image: url('<?php echo $img['image_large']['url']; ?>')"<?php } ?>>
 	<?php if ( $content ) { ?>
 		<div class="banner--content">
 			<div class="banner--text">
@@ -29,6 +30,6 @@ $size = get_field('banner_size');
 			<?php } ?>
 		</div>
 	<?php } ?>
-	<?php if ( $image['banner_image_large'] ) { ?><img loading="lazy" class="banner--image hidemobile" src="<?php echo $image['banner_image_large']['url']; ?>" alt="<?php echo $image['banner_image_large']['alt']; ?>" /><?php } ?>
-	<?php if ( $image['banner_image_mobile'] ) { ?><img loading="lazy" class="banner--image hidedesktop" src="<?php echo $image['banner_image_mobile']['url']; ?>" alt="<?php echo $image['banner_image_mobile']['alt']; ?>" /><?php } ?>
+	<?php if ( $img ) { ?><img loading="lazy" class="banner--image hidemobile" src="<?php echo $img['image_large']['url']; ?>" alt="<?php echo $img['image_large']['alt']; ?>" /><?php } ?>
+	<?php if ( $img ) { ?><img loading="lazy" class="banner--image hidedesktop" src="<?php echo $img['image_mobile']['url']; ?>" alt="<?php echo $img['image_large']['alt']; ?>" /><?php } ?>
 </section>
