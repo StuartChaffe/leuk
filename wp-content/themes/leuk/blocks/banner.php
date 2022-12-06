aaca588d4c4fbc2ea186005bb7115d61bdb36651
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-12-06 11:38:18 +0000

    Add video to banner

--- wp-content/themes/leuk/blocks/banner.php
+++ wp-content/themes/leuk/blocks/banner.php
@@ -8,9 +8,18 @@ $content = get_field('banner_content');
 $button = get_field('banner_button');
 $form = get_field('banner_form', false, false);
 $size = get_field('banner_size');
+$video = get_field('banner_video');
+
+// Build the  Shortcode
+$attr =  array(
+	'mp4'      => $video,
+	// 'poster'   => $video_poster,
+	'preload'  => 'auto',
+	'autoplay' => 'true'
+	);
 ?>
 
-<section class="banner<?php if ( $size == 'banner__small' ) { ?> banner__small<?php } ?>" <?php if ( $img ) { ?>style="background-image: url('<?php echo $img['image_large']['url']; ?>')"<?php } ?>>
+<section class="banner<?php if ( $size == 'banner__small' ) { ?> banner__small<?php } ?>" <?php if ( $img['image_large'] ) { ?>style="background-image: url('<?php echo $img['image_large']['url']; ?>')"<?php } ?>>
 	<?php if ( $content ) { ?>
 		<div class="banner--content">
 			<div class="banner--text">
@@ -30,6 +39,12 @@ $size = get_field('banner_size');
 			<?php } ?>
 		</div>
 	<?php } ?>
-	<?php if ( $img ) { ?><img loading="lazy" class="banner--image hidemobile" src="<?php echo $img['image_large']['url']; ?>" alt="<?php echo $img['image_large']['alt']; ?>" /><?php } ?>
-	<?php if ( $img ) { ?><img loading="lazy" class="banner--image hidedesktop" src="<?php echo $img['image_mobile']['url']; ?>" alt="<?php echo $img['image_large']['alt']; ?>" /><?php } ?>
+
+<?php if ($video) { ?>
+	<div class="banner--video">
+		<video src="<?php echo $video['url']; ?>" loop="false" muted="false" data-poster="" preload="" playsinline="" scrollspy="" autoplay="true"></video>
+	</div>
+<?php } ?>
+	<?php if ( $img['image_large'] ) { ?><img loading="lazy" class="banner--image hidemobile" src="<?php echo $img['image_large']['url']; ?>" alt="<?php echo $img['image_large']['alt']; ?>" /><?php } ?>
+	<?php if ( $img['image_mobile'] ) { ?><img loading="lazy" class="banner--image hidedesktop" src="<?php echo $img['image_mobile']['url']; ?>" alt="<?php echo $img['image_mobile']['alt']; ?>" /><?php } ?>
 </section>
